19427fb4c1036c9d7f603a38c2c27fceca934662
tejasjokin <61027009+tejasjokin@users.noreply.github.com>
2020-09-25 21:44:52 +0530

    Adding db

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require('knex')
+const bcrypt = require('bcryptjs');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'tejas@db',
+    database : 'mescoe'
+  }
+});
+
+db.withSchema('faculty').select('*').from('users').then(data => {
+	console.log(data);
+});
 
 const students = [
 	{
@@ -75,14 +91,25 @@ app.post('/signin/student', (req, res) => {
 	{
 		return res.status(400).json('Incorrect Submission');
 	}
-	if(email===students[0].email && password===students[0].password)
-	{
-		return res.json(students[0]);
-	}
-	else
-	{
-		return res.status(400).json('Wrong Credentials');
-	}
+	db.withSchema('student').select('email', 'hash').from('login')
+	.where('email', '=', email)
+	.then(data => {
+		const isValid = bcrypt.compareSync(password, data[0].hash);
+		if(isValid)
+		{
+			return db.withSchema('student').select('*').from('users')
+			.where('email', '=', email)
+			.then(user => {
+				res.json(user[0])
+			})
+			.catch(err => res.status(400).json('Unable to get User.'))
+		}
+		else
+		{
+			res.status(400).json('Wrong Credentials')
+		}
+	})
+	.catch(err => res.status(400).json('Wrong Credentials'))
 });
 
 app.post('/signin/faculty', (req, res) => {
@@ -91,28 +118,90 @@ app.post('/signin/faculty', (req, res) => {
 	{
 		return res.status(400).json('Incorrect Submission');
 	}
-	if(email===faculty[0].email && password===faculty[0].password)
-	{
-		return res.json(faculty[0]);
-	}
-	else
-	{
-		return res.status(400).json('Wrong Credentials');
-	}
+	db.withSchema('faculty').select('email', 'hash').from('login')
+	.where('email', '=', email)
+	.then(data => {
+		const isValid = bcrypt.compareSync(password, data[0].hash);
+		if(isValid)
+		{
+			return db.withSchema('faculty').select('*').from('users')
+			.where('email', '=', email)
+			.then(user => {
+				res.json(user[0])
+			})
+			.catch(err => res.status(400).json('Unable to get User.'))
+		}
+		else
+		{
+			res.status(400).json('Wrong Credentials')
+		}
+	})
+	.catch(err => res.status(400).json('Wrong Credentials'))	
 });
 
 app.post('/register/student', (req, res) => {
-	const new_user = req.body;
-	const new_student = {...new_user, joined: new Date};
-	students.push(new_student);
-	res.json(students[students.length-1]);
+	const {name, year, branch, div, prn, email, password} = req.body;
+	const salt = bcrypt.genSaltSync(10);
+	const hash = bcrypt.hashSync(password, salt);
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('student.login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('student.users')
+				.returning('*')
+				.insert({
+					name: name,
+					year: year,
+					branch: branch,
+					div: div,
+					prn: prn,
+					email: loginEmail[0],
+					joined: new Date()
+				})
+				.then(user => {
+					res.json(user[0]);
+				})
+			})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})
+	.catch(err => res.status(400).json('Unable to register'));
 })
 
 app.post('/register/faculty', (req, res) => {
-	const new_user = req.body;
-	const new_faculty = {...new_user, joined: new Date};
-	faculty.push(new_faculty);
-	res.json(faculty[faculty.length-1]);
+	const {name, employeeid, year, branch, prn, email, password} = req.body;
+	const salt = bcrypt.genSaltSync(10);
+	const hash = bcrypt.hashSync(password, salt);
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('faculty.login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('faculty.users')
+				.returning('*')
+				.insert({
+					name: name,
+					year: year,
+					branch: branch,
+					prn: prn,
+					email: loginEmail[0],
+					joined: new Date()
+				})
+				.then(user => {
+					res.json(user[0]);
+				})
+			})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})
+	.catch(err => res.status(400).json(err));
 })
 
 app.listen(3001, () => {
